d20d566fa480014824944c9243ed77220179d7a4
Blas Vinas <blasvinas@gmail.com>
2024-10-07 06:04:08 -0400

    add main.go

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/blasvinas/go_test/my_math"
+)
+
+func main() {
+	result := my_math.Add(5, 7)
+	fmt.Println(result)
+}
--- my_math/my_math.go
+++ my_math/my_math.go
@@ -1,5 +1,5 @@
 package my_math
 
-fun Add(a, b int) int {
-   return a + b
+func Add(a, b int) int {
+	return a + b
 }
